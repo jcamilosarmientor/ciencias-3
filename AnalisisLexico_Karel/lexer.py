58021f3724961cb3f50b01b0981751f133bdc2ed
OlgaMoyano <36648427+OlgaMoyano@users.noreply.github.com>
2018-09-29 21:05:25 -0500

    Update lexer.py

--- AnalisisLexico_Karel/lexer.py
+++ AnalisisLexico_Karel/lexer.py
@@ -1,77 +1,95 @@
 import ply.lex as lex
 
 
-reservada = (
-
-    # Palabras Reservadas
-    'AVANZA',
-    'APAGATE',
-##    'GIRA-IZQUIERDA'
-##    'COGE-ZUMBADOR'
-##    'DEJA-ZUMBADOR'
-    
+declaracion = (
+    'PROGRAMA',
+        
 )
-condicion = (
-    'SI',
-    'SINO',
-    'MIENTRAS',
+expresion = (
+    'IF',
+    'ITERATE',
+    'WHILE',
     'ENTONCES',
+    'TURNOFF',
+    'TURNLEFT',
+    'MOVE',
+    'PICKBEEPER',
+    'PUTBEEPER',
+    'RETURN',
+    'EXPVACIA',
 )
 
-##decisiones=(
-##'frente-libre',
-##'junto-a-zumbador',
-##'frente-bloqueado',
-##'no-junto-a-zumbador',
-##'orientado-al-oeste',
-##'izquierda-libre',
-##'algun-zumbador-en-la-mochila',
-##'no-orientado-al-norte',
-##'izquierda-bloqueada',
-##'ningun-zumbador-en-la-mochila',
-##'no-orientado-al-sur',
-##'derecha-libre',
-##'orientado-al-norte',
-##'no-orientado-al-este',
-##'derecha-bloqueada',
-##'orientado-al-sur',
-##'no-orientado-al-oeste',
-##)
 
-tokens =reservada+condicion+('NAME','NUMBER','PLUS','MINUS','TIMES','DIVIDE', 'EQUALS','SEPARADOR','GIRiZQ',)
+tokens =declaracion+expresion+('IDENTIFICADOR','TIPO','DECIMAL','ARGVACIO','TERMINO','CLAUSULAY','CLAUSULANO','ATOMICA','EQUALS','SEPARADOR','GIRIZQ','BLOQUE')
 
 t_ignore = ' \t'
-t_PLUS = r'SUM'
-t_MINUS = r'RES'
-t_TIMES = r'MUL'
-t_DIVIDE = r'DIV'
+t_ARGVACIO = r'\(\)'
+t_TERMINO=r'\|\|'
+t_CLAUSULAY = r'&&'
+t_CLAUSULANO = r'!'
+t_TIPO=r'define' or r'void'
 t_EQUALS = r'='
-t_NAME = r'[a-zA-Z_][a-zA-Z0-9_]'
-t_SEPARADOR='-'
-t_AVANZA='avanza'
-
+t_BLOQUE =r'\{'
+t_IDENTIFICADOR = r'[a-zA-Z_][a-zA-Z0-9_]*'
 
 lista = []
-def t_NUMBER(t):
+def t_DECIMAL(t):
     r'\d+'
     t.value = int(t.value)
     return t
 
-def t_SINO(t):
-    r'sino'
+def t_ITERATE(t):
+    r'iterate'
     return t
 
-def t_MIENTRAS(t):
-    r'mientras'
+def t_WHILE(t):
+    r'while'
     return t
 
 def t_ENTONCES(t):
     r'entonces'
     return t
 
-def t_SI(t):
-    r'si'
+def t_IF(t):
+    r'if'
     return t
+
+def t_ATOMICA (t):
+    r'iszero'
+    return t
+
+def t_TURNOFF (t):
+    r'turnoff'
+    return t
+
+def t_TURNLEFT (t):
+    r'turnleft'
+    return t
+
+def t_TURNLEFT (t):
+    r'turnleft'
+    return t
+
+def t_MOVE (t):
+    r'move'
+    return t
+
+def t_RETURN (t):
+    r'return'
+    return t
+
+def t_PICKBEEPER (t):
+    r'pickbeeper'
+    return t
+
+def t_EXPVACIA (t):
+    r';'
+    return t
+
+def t_PROGRAMA(t):
+    r'program'
+    return t
+
 
 # Error handling rule
 def t_error(t):
